2ecdf7ffcbc108f5809b34ab3ce49275750a9291
nhan <nhanpq@hpt.vn>
2023-08-18 18:15:45 +0700

    add prj3 (docker)

---
+++ prj3/src/main/java/com/example/demo/controller/AppController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.JsonObject;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AppController {
+
+    @GetMapping("")
+    public ResponseEntity<JsonObject> test(){
+        JsonObject res = new JsonObject();
+        res.addProperty("test", "test");
+        return new ResponseEntity<JsonObject>(res,HttpStatus.OK);
+    }
+    
+}
